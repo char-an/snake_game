37846799e8d0dfb2cba1588d85dd3061938ec098
char-an <156537912+char-an@users.noreply.github.com>
2024-07-25 22:24:59 +0530

    window

---
+++ GameWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+    public GameWindow() {
+        setTitle("Snake Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        add(new GamePanel());
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
